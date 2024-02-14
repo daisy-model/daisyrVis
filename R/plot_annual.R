d2a5c978720335827af1a708b48a6839852cd5e7
Silas Ørting <silas.orting@pm.me>
2024-02-14 11:38:24 +0100

    Add lint workflow + lint code (#1)

--- R/plot_annual.R
+++ R/plot_annual.R
@@ -1,21 +1,27 @@
 #' Make a bar plot of one or more variables from one or more dlfs
-#' @param dlfs  Either a list of Dlf or a single Dlf. If a list with of components, the names are
-#'              used for the legend.
+#' @param dlfs  Either a list of Dlf or a single Dlf. If a list with of
+#'              components, the names are used for the legend.
 #' @param variables  Either a list of variables to plot, or a single variable
 #' @param title_suffix  A string that is appended to the title of all subplots
 #' @return ggplot2
 #' @export
 #' @examples
 #' # Load dlf data with read_dlf
-#' data_dir <- file.path(system.file("extdata", package="daisyrVis"), 'annual/Annual-FN')
-#' scenarios <- list('2-2b'='HourlyP-Annual-FN-2-2b.dlf', '2-3b'='HourlyP-Annual-FN-2-3b.dlf',
-#'                   '2-4b'='HourlyP-Annual-FN-2-4b.dlf', '2-5b'='HourlyP-Annual-FN-2-5b.dlf')
-#' dlfs <- lapply(scenarios, function(fname) { read_dlf(file.path(data_dir, fname)) })
-#' 
+#' data_dir <- file.path(system.file("extdata", package="daisyrVis"),
+#'                       'annual/Annual-FN')
+#' scenarios <- list('2-2b'='HourlyP-Annual-FN-2-2b.dlf',
+#'                   '2-3b'='HourlyP-Annual-FN-2-3b.dlf',
+#'                   '2-4b'='HourlyP-Annual-FN-2-4b.dlf',
+#'                   '2-5b'='HourlyP-Annual-FN-2-5b.dlf')
+#' dlfs <- lapply(scenarios, function(fname) {
+#'     read_dlf(file.path(data_dir, fname))
+#' })
 #' # Be careful with variable names. R replaces '-' with '.'
-#' variables <- c('Matrix.Leaching', 'Crop.Uptake', 'Soil.Drain', 'Surface.Loss')
-#' plot_annual(dlfs, variables, ' - Annual Field Nitrogen')
+#' vars <- c('Matrix.Leaching', 'Crop.Uptake', 'Soil.Drain', 'Surface.Loss')
+#' plot_annual(dlfs, vars, ' - Annual Field Nitrogen')
 plot_annual <- function(dlfs, variables, title_suffix='') {
-    geom <- function() { ggplot2::geom_col(position='dodge') }
+    geom <- function() {
+        ggplot2::geom_col(position='dodge')
+    }
     daisyrVis::plot_many(dlfs, 'year', variables, geom, title_suffix)
 }
