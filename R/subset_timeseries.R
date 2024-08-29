cc5f4ff89f68f1c3ec5e11de38ab3d41e10cf2c0
Silas <silas@di.ku.dk>
2024-08-29 08:47:19 +0200

    Allow empty date_from/date_to

--- R/subset_timeseries.R
+++ R/subset_timeseries.R
@@ -1,15 +1,18 @@
-#' Select the subset of measurements in a specific time range
+#' Select the subset of measurements in a specific time range.
 #' @param dlfs  Either a list of Dlf or a single Dlf
 #' @param date_from Start date (included). ISO 8601 format (yyyy-mm-dd),
-#' e.g 1999-01-31
+#' e.g 1999-01-31. Can be the empty string "" in which case only dates after
+#' date_to are excluded
 #' @param date_to End date (included). ISO 8601 format (yyyy-mm-dd),
-#' e.g 1999-01-31
+#' e.g 1999-01-31. Can be the empty string "" in which case only dates before
+#' date_from are excluded
 #' @param time_from Start time (included). ISO 8601 format (hh:mm:ss),
 #' e.g. 07:54:20
 #' @param time_to End time (included). ISO 8601 format (hh:mm:ss),
 #' e.g. 07:54:20
 #' @param time_zone Name of time zone.
-#' @param time_col Name of time column in dlfs.
+#' @param time_col Name of time column in dlfs. Must be in POSIXct format. See
+#' `daisy_time_to_timestamp` for converting daisy time columns to POSIXct
 #' @return Dlfs with data slot containing the selected rows
 #' @export
 #' @examples
@@ -21,21 +24,41 @@
 #' data.frame(lapply(dlfs, function(dlf) { dlf$time}))
 #' dlfs <- subset_timeseries(dlfs, "1995-04-01", "1999-04-01", time_zone="CEST")
 #' data.frame(lapply(dlfs, function(dlf) { dlf$time}))
-subset_timeseries <- function(dlfs, date_from, date_to,
+subset_timeseries <- function(dlfs, date_from="", date_to="",
                               time_from="00:00:00", time_to="23:59:59",
                               time_zone="UTC", time_col="time") {
-
-    from <- as.POSIXct(paste(date_from, time_from), tz=time_zone,
-                       format="%Y-%m-%d %H:%M:%OS")
-    to <- as.POSIXct(paste(date_to, time_to), tz=time_zone,
-                     format="%Y-%m-%d %H:%M:%OS")
-    subset_ <- function(dlf) {
-        dlf@data <- dlf@data[from <= dlf[[time_col]] & dlf[[time_col]] <= to, ]
-        dlf
-    }
-    if (is.list(dlfs)) {
-        lapply(dlfs, subset_)
+    if (nchar(date_from) == 0 && nchar(date_to) == 0) {
+        dlfs
     } else {
-        subset_(dlfs)
+        if (nchar(date_from) > 0) {
+            from <- as.POSIXct(paste(date_from, time_from), tz=time_zone,
+                               format="%Y-%m-%d %H:%M:%OS")
+        }
+        if (nchar(date_to) > 0) {
+            to <- as.POSIXct(paste(date_to, time_to), tz=time_zone,
+                             format="%Y-%m-%d %H:%M:%OS")
+        }
+        if (nchar(date_from) == 0) {
+            subset_ <- function(dlf) {
+                dlf@data <- dlf@data[dlf[[time_col]] <= to, ]
+                dlf
+            }
+        } else if (nchar(date_to) == 0) {
+            subset_ <- function(dlf) {
+                dlf@data <- dlf@data[from <= dlf[[time_col]], ]
+                dlf
+            }
+        } else {
+            subset_ <- function(dlf) {
+                dlf@data <-
+                    dlf@data[from <= dlf[[time_col]] & dlf[[time_col]] <= to, ]
+                dlf
+            }
+        }
+        if (is.list(dlfs)) {
+            lapply(dlfs, subset_)
+        } else {
+            subset_(dlfs)
+        }
     }
 }
