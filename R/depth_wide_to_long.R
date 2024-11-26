e61f7775905b55ac22c27fee30113b7e15f3890a
Silas <silas@di.ku.dk>
2024-11-26 08:51:47 +0100

    Make depth_wide_to_long work on list of dlfs + allow setting depth unit

--- R/depth_wide_to_long.R
+++ R/depth_wide_to_long.R
@@ -4,10 +4,11 @@
 #'   <var_name>_<depth-below-surface>
 #' e.g. "q_100"
 #'
-#' @param dlf An S4 object of class Dlf
+#' @param dlf An S4 object of class Dlf or a list of Dlf objects
 #' @param var_name Name of variable in columns
 #' @param time_name Name of time column
 #' @param depth_name Name to use for new depth column
+#' @param depth_unit Unit of depth
 #' @return An S4 object of class Dlf
 #'
 #' The data of rhe returned Dlf object contains one row for each time/depth
@@ -24,26 +25,32 @@
 #' dlf <- depth_wide_to_long(dlf, "q")
 #' head(dlf@data)
 depth_wide_to_long <- function(dlf, var_name, time_name="time",
-                               depth_name="z") {
-    names <- colnames(dlf@data)
-    varying <- names[startsWith(names, var_name)]
-    data <- reshape(
-        dlf@data, varying, direction="long", sep="..AT..", timevar=depth_name,
-        new.row.names=NULL
-    )[c(depth_name, "time", var_name)]
-    data[, depth_name] <- as.numeric(gsub("_", "-", data[, depth_name]))
-    if (time_name != "time") {
-        colnames(data)[2] <- time_name
-    }
-    rownames(data) <- seq(1, nrow(data))
-    if (time_name %in% dlf@units) {
-        time_unit <- dlf@units[time_name]
+                               depth_name="z", depth_unit="unknown") {
+    if (is.list(dlf)) {
+        lapply(dlf, function(dlf_) {
+            depth_wide_to_long(dlf_, var_name, time_name, depth_name)
+        })
     } else {
-        time_unit <- ""
+        names <- colnames(dlf@data)
+        varying <- names[startsWith(names, var_name)]
+        data <- reshape(
+            dlf@data, varying, direction="long", sep="..AT..",
+            timevar=depth_name, new.row.names=NULL
+        )[c(depth_name, "time", var_name)]
+        data[, depth_name] <- as.numeric(gsub("_", "-", data[, depth_name]))
+        if (time_name != "time") {
+            colnames(data)[2] <- time_name
+        }
+        rownames(data) <- seq(1, nrow(data))
+        if (time_name %in% dlf@units) {
+            time_unit <- dlf@units[time_name]
+        } else {
+            time_unit <- ""
+        }
+        units <- data.frame(z=depth_unit,
+                            time=time_unit,
+                            q=dlf@units[varying[1]])
+        colnames(units) <- colnames(data)
+        new("Dlf", header=dlf@header, units=units, data=data)
     }
-    units <- data.frame(z="unknown-depth-unit",
-                        time=time_unit,
-                        q=dlf@units[varying[1]])
-    colnames(units) <- colnames(data)
-    new("Dlf", header=dlf@header, units=units, data=data)
 }
