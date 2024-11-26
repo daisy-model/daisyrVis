22842fd65ed25e943975dae114db8df9ced4f167
Silas <silas@di.ku.dk>
2024-11-26 14:18:17 +0100

    Guess time cols if none provided

--- R/daisy_time_to_timestamp.R
+++ R/daisy_time_to_timestamp.R
@@ -9,6 +9,9 @@
 #' @param drop_daisy_time_cols If TRUE drop the year, month, day, hour columns
 #' @return An S4 object of class Dlf
 #'
+#' If all of year_col, month_col, day_col and hour_col are NULL, then an attempt
+#' is made to guess the relevant names.
+#'
 #' The returned object is identical to the dlf, except that the Columns 'year',
 #' 'month', 'mday' and 'hour' are dropped and replaced with a column containing
 #' the corresponding POSIXct timestamp
@@ -22,9 +25,8 @@
 #' dlf@data[1,]
 #' dlf <- daisy_time_to_timestamp(dlf)
 #' dlf@data[1,]
-daisy_time_to_timestamp <- function(dlf, time_col_name='time', year_col="year",
-                                    month_col="month", day_col="mday",
-                                    hour_col="hour",
+daisy_time_to_timestamp <- function(dlf, time_col_name='time', year_col=NULL,
+                                    month_col=NULL, day_col=NULL, hour_col=NULL,
                                     drop_daisy_time_cols=FALSE) {
     if (is.list(dlf)) {
         lapply(dlf, function(dlf_) {
@@ -32,26 +34,20 @@ daisy_time_to_timestamp <- function(dlf, time_col_name='time', year_col="year",
                                     day_col, hour_col, drop_daisy_time_cols)
         })
     } else {
+        format_strings <- c(year="%Y", month="%m", day="%d", hour="%H")
         data <- dlf@data
-        time_cols <- c(year_col, month_col, day_col, hour_col)
-        format_string <- c()
-        if (!is.null(year_col)) {
-            format_string <- c(format_string, "%Y")
+        time_cols <- c(year=year_col, month=month_col, day=day_col,
+                       hour=hour_col)
+        if (is.null(time_cols)) {
+            time_cols <- guess_time_cols(colnames(data))
         }
-        if (!is.null(month_col)) {
-            format_string <- c(format_string, "%m")
-        }
-        if (!is.null(day_col)) {
-            format_string <- c(format_string, "%d")
-        }
-        if (!is.null(hour_col)) {
-            format_string <- c(format_string, "%H")
-        }
-        format_string <- paste(format_string, collapse=" ")
+        format_string <- paste(format_strings[names(time_cols)], collapse=" ")
         if (!all(time_cols %in% colnames(data))) {
-            stop(paste("Missing time columns.",
-                       time_cols[!(time_cols %in% colnames(data))],
-                       "not in", colnames(data)), sep=" ")
+            stop(paste0("Missing time columns: '",
+                        paste(time_cols[!(time_cols %in% colnames(data))],
+                              collapse=" "),
+                        "' not in '",
+                        paste(colnames(data), collapse=" "), "'"))
         }
         raw_ts <- apply(data[, time_cols], 1, function(row) {
             paste(row, collapse=" ")
@@ -65,4 +61,21 @@ daisy_time_to_timestamp <- function(dlf, time_col_name='time', year_col="year",
         units[time_col_name] <- ''
         new('Dlf', header=dlf@header, units=units, data=data)
     }
+}
+
+guess_time_cols <- function(column_names) {
+    time_cols <- list()
+    candidates <- list(year=c("year"),
+                       month=c("month"),
+                       day=c("mday", "day"),
+                       hour=c("hour"))
+    for (key in names(candidates)) {
+        for (candidate in candidates[[key]]) {
+            if (candidate %in% column_names) {
+                time_cols[[key]] <- candidate
+                break
+            }
+        }
+    }
+    unlist(time_cols)
 }
